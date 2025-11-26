860168972cf0f9cd713f06c75be5e2454e27eb4f
Alex Smagin <alexsmagin1@gmail.com>
2025-11-26 11:15:30 -0600

    feat: add responsiveness for page not found

--- frontend/src/pages/PageNotFound/PageNotFound.styled.tsx
+++ frontend/src/pages/PageNotFound/PageNotFound.styled.tsx
@@ -11,12 +11,19 @@ export const PageNotFoundWrapper = styled.div`
   gap: 2.4rem;
   align-items: center;
   justify-content: center;
-  padding-top: -7.2rem;
+  padding: 0 2.4rem;
 `;
 
 export const PageNotFoundImg = styled.img`
   width: 100%;
   max-width: 60rem;
+  padding: 0 2.4rem;
+
+
+  @media (min-width: 768px) {
+    padding: 0;
+    max-width: 60rem;
+  }
 `;
 
 export const PageNotFoundTitleWrapper = styled.div`
@@ -26,21 +33,32 @@ export const PageNotFoundTitleWrapper = styled.div`
 `;
 
 export const PageNotFoundTitle = styled.h2`
-  font-size: 2.4rem;
+  font-size: 1.8rem;
   font-weight: 700;
   color: ${(p) => p.theme.colors.mainTextColor};
   font-style: ${primaryFont};
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 2.4rem;
+  }
 `;
 
 export const PageNotFoundText = styled.p`
-  font-size: 1.6rem;
+  font-size: 1.4rem;
   color: ${(p) => p.theme.colors.secondaryTextColor};
   font-style: ${primaryFont};
   font-weight: 400;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    text-align: left;
+    font-size: 1.6rem;
+  }
 `;
 
 export const PageNotFoundLink = styled(Link)`
-  font-size: 1.6rem;
+  font-size: 1.4rem;
   color: ${(p) => p.theme.colors.linkTextColor};
   font-style: ${primaryFont};
   font-weight: 700;
@@ -66,5 +84,9 @@ export const PageNotFoundLink = styled(Link)`
       width: 100%;
       background-color: ${(p) => p.theme.colors.linkTextHoverColor};
     }
+  }
+
+  @media (min-width: 768px) {
+    font-size: 1.6rem;
   }
 `;
